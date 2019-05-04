2fd29577203ed9a8485cf4af136cd3bac1d556c2
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 01:55:45 +0300

    Update arissploit.py

--- arissploit.py
+++ arissploit.py
@@ -54,23 +54,32 @@ def show_graphic():
     menu = random.randrange(2, 15)
     if menu == 2:
     os.system("python2 banner/banner.py")
-    if menu == 3:
+    return
+if menu == 3:
     os.system("python2 banner/banner1.py")
-    if menu == 4:
+    return
+if menu == 4:
     os.system("python2 banner/banner2.py")
-    if menu == 5:
+    return
+if menu == 5:
     os.system("python2 banner/banner3.py")
-    if menu == 6:
+    return 
+if menu == 6:
     os.system("cat banner/banner4.py")
-    if menu == 7:
+    return 
+if menu == 7:
     os.system("python2 banner/banner5.py")
-    if menu == 8:
+    return 
+if menu == 8:
     os.system("python2 banner/banner6.py")
-    if menu == 9:
+    return 
+if menu == 9:
     os.system("cat banner/banner7.py")
-    if menu == 10:
+    return 
+if menu == 10:
     os.system("python2 banner/banner8.py")
-    if menu == 11:
+    return 
+if menu == 11:
     os.system("cat banner/banner9.py")
     
 show_graphic()

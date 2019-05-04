ae4d6e22b3bdd24c7daf66fc1a28fb7474c32cd3
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 02:11:21 +0300

    Update config.py

--- banner/config.py
+++ banner/config.py
@@ -5,33 +5,25 @@ import random
 import os as sistema
 
 menu = random.randrange(2, 15)
+    
     if menu == 2:
     os.system("python2 banner/banner.py")
-    return
-    if menu == 3:
+    elif menu == 3:
     os.system("python2 banner/banner1.py")
-    return
-    if menu == 4:
+    elif menu == 4:
     os.system("python2 banner/banner2.py")
-    return
-    if menu == 5:
+    elif menu == 5:
     os.system("python2 banner/banner3.py")
-    return 
-    if menu == 6:
+    elif menu == 6:
     os.system("cat banner/banner4.py")
-    return 
-    if menu == 7:
+    elif menu == 7:
     os.system("python2 banner/banner5.py")
-    return 
-    if menu == 8:
+    elif menu == 8:
     os.system("python2 banner/banner6.py")
-    return 
-    if menu == 9:
+    elif menu == 9:
     os.system("cat banner/banner7.py")
-    return 
-    if menu == 10:
+    elif menu == 10:
     os.system("python2 banner/banner8.py")
-    return 
-    if menu == 11:
+    elif menu == 11:
     os.system("cat banner/banner9.py")
     

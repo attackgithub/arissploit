ae7437f3ad5e68a336994faeaa0c2cb376ae0639
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 02:06:29 +0300

    Create config.py

---
+++ banner/config.py
@@ -0,0 +1,31 @@
+menu = random.randrange(2, 15)
+    if menu == 2:
+    os.system("python2 banner/banner.py")
+    return
+    if menu == 3:
+    os.system("python2 banner/banner1.py")
+    return
+    if menu == 4:
+    os.system("python2 banner/banner2.py")
+    return
+    if menu == 5:
+    os.system("python2 banner/banner3.py")
+    return 
+    if menu == 6:
+    os.system("cat banner/banner4.py")
+    return 
+    if menu == 7:
+    os.system("python2 banner/banner5.py")
+    return 
+    if menu == 8:
+    os.system("python2 banner/banner6.py")
+    return 
+    if menu == 9:
+    os.system("cat banner/banner7.py")
+    return 
+    if menu == 10:
+    os.system("python2 banner/banner8.py")
+    return 
+    if menu == 11:
+    os.system("cat banner/banner9.py")
+    

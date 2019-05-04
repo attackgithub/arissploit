eef1f1579c7a954c5f47e67ba0c38a9f50aad42c
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 02:32:10 +0300

    Update config.py

--- banner/config.py
+++ banner/config.py
@@ -14,16 +14,8 @@ import inspect
 import base64
 import io
 import trace
-        PURPLE = '\033[95m'
-        CYAN = '\033[96m'
-        DARKCYAN = '\033[36m'
-        BLUE = '\033[94m'
-        GREEN = '\033[92m'
-        YELLOW = '\033[93m'
-        RED = '\033[91m'
-        BOLD = '\033[1m'
-        UNDERL = '\033[4m'
-        ENDC = '\033[0m'
+        
+
     menu = random.randrange(2, 15)
     if menu == 2:
         print(YELLOW + r"""

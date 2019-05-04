0c87f8429db10aab320417dc188f14f569550fae
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 02:00:21 +0300

    Update arissploit.py

--- arissploit.py
+++ arissploit.py
@@ -55,31 +55,31 @@ def show_graphic():
     if menu == 2:
     os.system("python2 banner/banner.py")
     return
-if menu == 3:
+    if menu == 3:
     os.system("python2 banner/banner1.py")
     return
-if menu == 4:
+    if menu == 4:
     os.system("python2 banner/banner2.py")
     return
-if menu == 5:
+    if menu == 5:
     os.system("python2 banner/banner3.py")
     return 
-if menu == 6:
+    if menu == 6:
     os.system("cat banner/banner4.py")
     return 
-if menu == 7:
+    if menu == 7:
     os.system("python2 banner/banner5.py")
     return 
-if menu == 8:
+    if menu == 8:
     os.system("python2 banner/banner6.py")
     return 
-if menu == 9:
+    if menu == 9:
     os.system("cat banner/banner7.py")
     return 
-if menu == 10:
+    if menu == 10:
     os.system("python2 banner/banner8.py")
     return 
-if menu == 11:
+    if menu == 11:
     os.system("cat banner/banner9.py")
     
 show_graphic()

bdd528977384fd206f04e39624517b97a352fb5a
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-05 02:05:56 +0300

    Update arissploit.py

--- arissploit.py
+++ arissploit.py
@@ -51,37 +51,7 @@ B = '\033[1;34m' #Blue
 E = '\033[0m' # End
 
 def show_graphic():
-    menu = random.randrange(2, 15)
-    if menu == 2:
-    os.system("python2 banner/banner.py")
-    return
-    if menu == 3:
-    os.system("python2 banner/banner1.py")
-    return
-    if menu == 4:
-    os.system("python2 banner/banner2.py")
-    return
-    if menu == 5:
-    os.system("python2 banner/banner3.py")
-    return 
-    if menu == 6:
-    os.system("cat banner/banner4.py")
-    return 
-    if menu == 7:
-    os.system("python2 banner/banner5.py")
-    return 
-    if menu == 8:
-    os.system("python2 banner/banner6.py")
-    return 
-    if menu == 9:
-    os.system("cat banner/banner7.py")
-    return 
-    if menu == 10:
-    os.system("python2 banner/banner8.py")
-    return 
-    if menu == 11:
-    os.system("cat banner/banner9.py")
-    
+    os.system("python2 banner/config.py")
 show_graphic()
 print
 print ""+N+"       =[ "+O+"Arissploit v.1 by Entynetproject             "+N+"]"
